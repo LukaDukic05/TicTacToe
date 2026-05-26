3eaae552d191175a170266e9011f5c9679705125
LukaDukic05 <wi25b178@technikum-wien.at>
2026-05-26 15:12:44 +0200

    Add getCells method

--- src/main/java/org/example/Board.java
+++ src/main/java/org/example/Board.java
@@ -1,1 +1,12 @@
 
+package org.example;
+public class Board{
+    private char [][] cells;
+
+    public Board(){
+        cells=new char[3][3];
+    }
+    public char[][] getCells(){
+        return cells;
+    }
+}
